8e6f82fb332696112f797c95a2d1a36146e3a34c
Amit Gajare <48202924+Amitgajare2@users.noreply.github.com>
2020-05-14 20:54:34 +0530

    Stylish Instagram Name
    How To Install Stylish Instagram Name Generator Tool In Termux

---
+++ style.sh
@@ -0,0 +1,67 @@
+#! /bin/bash/
+# instagram stylish Name Generator
+clear
+echo -e "\033[01;33;41m Instagram Stylish Names \033[0m "
+echo
+echo -e "\033[01;32m Enter Name \033[0m :- \c "
+read name
+echo
+echo "♡*:.｡.$name.｡.:*♡"
+echo
+echo "✿*:･$name･:*✿"
+echo
+echo "✧*:.｡$name｡.:*✧"
+echo
+echo "▂▃▅▆ $name ▆▅▃▂"
+echo
+echo "▂ ▃ ▄ ▅ $name ▅ ▄ ▃ ▂ "
+echo
+echo "★彡 $name 彡★"
+echo
+echo "▄︻┻═┳一 $name "
+echo
+echo "⊱⋅ $name ⋅⊰"
+echo
+echo "●❯ $name ❮●"
+echo
+echo "▣━━◤ $name ◢━━▣"
+echo
+echo "×•-•⟮ $name ⟯•-•×"
+echo
+echo "• • • $name • • •" 
+echo
+echo "$name ─═┳︻"
+echo
+echo "↬↬ $name ↫↫"
+echo
+echo "➶➶ $name ➷➷"
+echo
+echo "$name ツ"
+echo
+echo "$name 【ツ】"
+echo
+echo "╰☆ $name ☆╮"
+echo
+echo "꧁$name꧂"
+echo
+echo "ıllı $name ıllı"
+echo
+echo "༒$name༒"
+echo
+echo "▀▄▀$name▀▄▀"
+echo
+echo "‡$name‡"
+echo
+echo "✎ $name"
+echo
+echo "♛ $name"
+echo
+echo "$name ➢"
+echo
+echo "$name ඏ"
+echo
+echo "♧ $name"
+echo
+echo "♔ $name"
+echo
+
